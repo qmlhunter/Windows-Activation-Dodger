8fc2e2a3186fd0a1c0a6cfc023a8ee1c17e1b3d0
Quinntavich .IND <46363605+QMLHUNTER@users.noreply.github.com>
2022-01-21 13:56:48 -0800

    Stable

---
+++ Watermark Remover/ActivationDodger.bat
@@ -0,0 +1,4 @@
+@echo off
+cscript AppData.vbs "Watermark is being removed... (Disclaimer: This does not mean your pirating little swine arse owns a copy of Windows)"
+cscript WatermarkRemover.vbs
+start "" https://github.com/QMLHUNTER/Windows-Activation-Dodger
