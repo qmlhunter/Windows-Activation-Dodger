24d1561bbf6d7d5c0cf51cfd5293ae3b30e3c26b
Quinntavich <46363605+QMLHUNTER@users.noreply.github.com>
2022-01-21 18:39:39 -0800

    Add files via upload

--- Activation Dodger (Raw)/ActivationDodger.bat
+++ Activation Dodger (Raw)/ActivationDodger.bat
@@ -1,6 +1,6 @@
 @echo off
 cscript AppData.vbs "Watermark is being removed... (Disclaimer: This does not mean that your pirating swine arse owns a copy of Windows)"
 wait 500
-cscript DirCpy.vbs "Startup folder is in %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup (For Windows 10)
+cscript DirCpy.vbs "Startup folder is in %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 cscript Wrapper.vbs
 start "" https://github.com/QMLHUNTER/Windows-Activation-Dodger
