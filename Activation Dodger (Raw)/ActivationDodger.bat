09455abaf68cefd392a6399e9b9d342f183a1608
Quinntavich <46363605+qmlhunter@users.noreply.github.com>
2022-04-08 11:03:04 -0700

    Update ActivationDodger.bat

--- Activation Dodger (Raw)/ActivationDodger.bat
+++ Activation Dodger (Raw)/ActivationDodger.bat
@@ -1,6 +1,8 @@
 @echo off
 cscript AppData.vbs "Watermark is being removed... (Disclaimer: This does not mean that your pirating swine arse owns a copy of Windows)"
 wait 500
+cscript Appdata.vbs "Donate to my patreon @ Patreon.com/quinnfix"
+wait 500
 cscript DirCpy.vbs "Startup folder is in %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 cscript Wrapper.vbs
 start "" https://www.microsoft.com/en-ca/d/windows-10-home/d76qx4bznwk4/1nt3?rtc=2&activetab=pivot:overviewtab
